61f43cba5269b9ab9c32d4144de1a4291896e7d7
mateusscm <mateus.miranda@hotmail.com.br>
2020-12-20 20:57:05 -0300

    ranking page almost done

--- src/views/RankingPage/RankingPage.js
+++ src/views/RankingPage/RankingPage.js
@@ -1,6 +1,48 @@
-import React from "react";
-import { ContainerPage } from "./RankingPage.style";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { Banner, ContainerPage, ContainerRank } from "./RankingPage.style";
+
+import banner from "../../assets/banner2.png";
+import MiniCard from "../../components/MiniCard";
+import Loading from "../../components/Loading";
 
 export default function RankingPage() {
-  return <ContainerPage>Ranking</ContainerPage>;
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    axios
+      .get("http://localhost:5000/startups/rate")
+      .then(async (res) => {
+        console.log(res);
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.log(err);
+        setLoading(false);
+      });
+  }, []);
+
+  return (
+    <>
+      {loading && <Loading />}
+      <ContainerPage>
+        <Banner src={banner} />
+        <ContainerRank>
+          <div className="content-rank">
+            <span>Proposta</span>
+            <MiniCard />
+          </div>
+          <div className="content-rank">
+            <span>Apresentação / Pitch</span>
+            <MiniCard />
+          </div>
+          <div className="content-rank">
+            <span>Desenvolvimento</span>
+            <MiniCard />
+          </div>
+        </ContainerRank>
+      </ContainerPage>
+    </>
+  );
 }
--- src/views/RankingPage/RankingPage.style.js
+++ src/views/RankingPage/RankingPage.style.js
@@ -1,20 +1,62 @@
 import styled from "styled-components";
 
-export const ContainerPage = styled.div`
-  /* width: 1000px;
-  background-color: transparent;
-  height: auto;
-  transition: width 0.5s ease-in-out;
+export const ContainerPage = styled.div``;
+
+export const ContainerRank = styled.div`
+  display: flex;
+  margin-top: 16px;
+  align-items: baseline;
+
+  & > div {
+    flex: 1;
+  }
+
+  .content-rank:first-child {
+    margin-right: 20px;
+  }
+
+  .content-rank:nth-child(3) {
+    margin-left: 20px;
+  }
+
+  .content-rank {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+
+    & > span {
+      font-weight: bold;
+      font-size: 1.2rem;
+    }
+  }
 
   @media screen and (max-width: 1024px) {
-    width: 700px;
-  }
+    flex-direction: column;
 
-  @media screen and (max-width: 800px) {
-    width: 600px;
-  }
+    .content-rank:first-child {
+      margin-right: 0px;
+    }
 
-  @media screen and (max-width: 600px) {
-    width: 90vw;
-  } */
+    .content-rank:nth-child(3) {
+      margin-left: 0px;
+    }
+
+    & > div {
+      width: 100%;
+      margin-top: 20px;
+
+      &:first-child {
+        margin-top: 0px;
+      }
+    }
+  }
+`;
+
+export const Banner = styled.div`
+  width: 100%;
+  height: 210px;
+  background-image: url(${(props) => props.src});
+  background-size: cover;
+  background-repeat: no-repeat;
 `;
