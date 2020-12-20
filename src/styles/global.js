71055dbf3217a5e60914062aa955fcea8e74a680
mateusscm <mateus.miranda@hotmail.com.br>
2020-12-20 13:35:07 -0300

    changing global style

--- src/styles/global.js
+++ src/styles/global.js
@@ -18,4 +18,23 @@ body {
     display: flex;
     justify-content: center;
     }
+
+    main {
+        width: 1000px;
+        background-color: transparent;
+        height: auto;
+        transition: width 0.5s ease-in-out;
+
+        @media screen and (max-width: 1024px) {
+            width: 700px;
+        }
+
+        @media screen and (max-width: 800px) {
+            width: 600px;
+        }
+
+        @media screen and (max-width: 600px) {
+            width: 90vw;
+        }
+    }
 `;
