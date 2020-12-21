22f3071444fa909a83baeefbbff6e7a84009f681
mateusscm <mateus.miranda@hotmail.com.br>
2020-12-21 11:35:38 -0300

    data from startups in parent component

--- src/components/MainRoutes/index.js
+++ src/components/MainRoutes/index.js
@@ -1,17 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Switch, Route } from "react-router-dom";
+import axios from "axios";
+import Loading from "../Loading/index";
 
 export default function MainRoutes({ routes }) {
+  const [loading, setLoading] = useState(false);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    setLoading(true);
+    axios
+      .get("http://localhost:5000/startups")
+      .then(async (res) => {
+        setData(res.data["startups"]);
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.log(err);
+        setLoading(false);
+      });
+  }, []);
+
   return (
     <main>
+      {loading && <Loading />}
       <Switch>
         {routes.map((route) => (
           <Route
-            key={Math.random()}
+            key={route.path}
             exact={route.exact}
             path={route.path}
             render={(props) => {
-              return <route.component {...props} routes={routes} />;
+              return <route.component {...props} routes={routes} data={data} />;
             }}
           />
         ))}
